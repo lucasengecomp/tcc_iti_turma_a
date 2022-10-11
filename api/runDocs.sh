7ff11cce69d221e841257932be7cef54212b369e
Rodrigo Bueno <rodrigobuenorrs@gmail.com>
2022-10-11 02:50:25 -0300

    Melhoria em processo de geração de docs

--- api/runDocs.sh
+++ api/runDocs.sh
@@ -1,5 +1,7 @@
 
-pdoc --html --output-dir server/blueprints/docs/ . --force
+pdoc --html --output-dir server/blueprints/docs/ --force --template-dir server/blueprints/docs/  .
+
+rm -rf server/blueprints/docs/static
 
 mv server/blueprints/docs/project server/blueprints/docs/static
 
