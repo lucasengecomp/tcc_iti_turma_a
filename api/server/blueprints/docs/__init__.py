4a809aa7596e33c0b91ffaf02a1bb3736717b216
Rodrigo Bueno <rodrigobuenorrs@gmail.com>
2022-10-11 02:52:58 -0300

    Navegação dinamica em arquivos de doc

--- api/server/blueprints/docs/__init__.py
+++ api/server/blueprints/docs/__init__.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-from flask import request, render_template, render_template_string, make_response
+from flask import request, redirect, make_response
 from flask_restx import Namespace, Resource, reqparse
 
 ns = Namespace('docs',
@@ -9,14 +9,50 @@ ns = Namespace('docs',
 
 
 @ns.route('')
-class docsTemplate(Resource):
+class docsHome(Resource):
 
     @ns.response(200, 'Documentação Renderizada')
     @ns.response(500, 'Internal Server error')
     def get(self):
 
-        #templates = glob.glob(os.path.join(os.path.dirname(__file__), "static", "**", "*.html"))
         template = os.path.join(os.path.dirname(__file__), "static", "index.html")
+
+        with open(template, "r") as f:
+            return make_response(f.read(), 200)
+
+
+@ns.route('/<path:basePath>/project/<path:page>')
+class docsLongLinkFixer(Resource):
+    
+    @ns.response(200, 'Documentação Renderizada')
+    @ns.response(500, 'Internal Server error')
+    def get(self, basePath, page):
+
+        return redirect("/docs/{}".format(page))
+
+
+@ns.route('/project/<path:page>')
+class docsLinkFixer(Resource):
+
+    @ns.response(200, 'Documentação Renderizada')
+    @ns.response(500, 'Internal Server error')
+    def get(self, page):
+
+        return redirect("/docs/{}".format(page))
+
+@ns.route('/<path:page>')
+class docsTemplate(Resource):
+
+    @ns.response(200, 'Documentação Renderizada')
+    @ns.response(500, 'Internal Server error')
+    def get(self, page):
+
+        print("2")
+        print(page)
+
+        pages = page.split("/")
+
+        template = os.path.join(os.path.dirname(__file__), "static", *pages)
 
         with open(template, "r") as f:
             return make_response(f.read(), 200)
